04d158699ab046e3bba329181737fd59d7f997ab
chensong <chensongbj25@gmail.com>
2021-12-03 12:06:00 +0800

    [client_device]

---
+++ cloud_platform/client_device/cgm_command.cpp
@@ -0,0 +1,120 @@
+﻿/***********************************************************************************************
+					created: 		2019-05-13
+
+					author:			chensong
+
+					purpose:		gm_command
+************************************************************************************************/
+
+#include "cgm_command.h"
+#include "clog.h"
+//#include "csend_msgc2s.h"
+//#include "cfriend_circle_table_mgr.h"
+namespace chen {
+	cgm_command::cgm_command()
+	{
+	}
+	cgm_command::~cgm_command()
+	{
+	}
+	bool cgm_command::init()
+	{
+		//using namespace csend_msgc2s;
+		//_register_command("help", &help, "[cmd]");
+		//_register_command("gm", &send_gm, "<cmd>");
+		//_register_command("login", &send_login, "<login>");
+
+		//// friend circle
+		//_register_command("friend_create_table", &send_friend_create_table, "<user_id> <circle_id> <room_name> <smallblind> <buyin> <maxplayers> <operatetime> <tabletype> <ante> <privatetable>  <privatekey>");
+		//_register_command("friend_delete_table", &send_friend_delete_table, "<user_id> <circle_id> <table_id> <table_type>");
+		//_register_command("friend_modify_table_info", &send_friend_modify_table, "<extra> <user_id> <circle_id> <table_id> <room_name> <small_blind> <maxplayers> <operatetime> <tabletype> <tablekey> <private_table> <game_type>");
+		//_register_command("match_player_joined_list", &send_match_player_joined_list, "<user_id>");
+
+  //      // join_table 35103  133741  35103 2 chensong 35103 9 2 2 1
+		////game 
+		//_register_command("join_table", &send_join_table, "<extra> <user_id> <game_id> <match_type> <game_user_name> <proxy_user_id> <circle_id> <private_key> <game_type> <join_type>");
+		//_register_command("leave_table", &send_leave_table, "<extra> <user_id> <game_id> <match_type> <circle_id>  <game_type>");
+
+		//// game logic game_info 30809  131857  30809 2  robot100 2
+		//_register_command("game_info", &send_game_info, "<extra> <user_id> <game_id> <match_type> <user_name> <game_type>");
+		//_register_command("game_timer_info", &send_game_timer_info, "<extra> <user_id> <game_id> <match_type> <game_type>");
+		//_register_command("game_bonus", &send_game_bonus, "<extra> <user_id> <game_id> <game_type>");
+		//_register_command("game_action", &send_game_action, "<extra> <user_id> <game_id> <match_type> <chair_id> <action_type> <coin_count> <game_type>");
+		//_register_command("game_player_ranking", &send_game_player_ranking, "<extra> <user_id> <game_id> <match_type> <game_type>");
+		//_register_command("game_player_deregister_push_match_info", &send_game_player_deregister_push_match_info, "<extra> <user_id> <game_id> <game_type>");
+		//_register_command("game_show_hand_card", &send_game_show_hand_card, "<extra> <user_id> <game_id> <game_type> <count> <hand_cardss>");
+
+		//// lookon
+		//_register_command("game_lookon", &send_game_lookon, "<extra> <user_id> <game_id> <game_type>  <game_type>");
+		//_register_command("game_cancal_lookon", &send_game_cancal_lookon, "<extra> <user_id> <game_id> <match_type> <game_type>");
+
+		//_register_command("game_player_back_chair", &send_game_player_back_chair, "<extra> <user_id> <game_id> <game_type> <chair_id>");
+		//_register_command("game_add_score", &send_game_add_score, "<extra> <user_id> <game_id> <game_type> <multiple>");
+		//_register_command("game_lookon_players_data", &send_game_lookon_players_data, "<extra> <user_id> <match_id> <match_type>");
+		////game_player_device 30809  131857  30809 2   2
+  //      _register_command("game_player_device", &send_game_player_device, "<extra> <player_id> <match_id> <match_type><web_device>");
+//		_register_command("match_add_chip", &send_match_add_chip, "<extra> <user_id> <match_id> <match_type> <num>");
+	//	_register_command("match_player_signup", &send_match_player_signup, "<extra> <player_id>");
+     //   _register_command("match_player_operationtimes", &send_match_player_operationtimes, "<extra> <player_id><match_id><match_type>");
+    //    _register_command("match_player_add_operationtimes", &send_match_player_add_operationtimes, "<extra> <player_id><match_id><match_type>");
+		return true;
+	}
+	void cgm_command::destroy()
+	{
+		m_gm_commands.clear();
+	}
+	void cgm_command::parse_commands(const char * text, csession * session_ptr)
+	{
+		if (text == NULL || text[0] == 0)
+		{
+			return;
+		}
+		SYSTEM_LOG("[commands]cmd = %s", text);
+		ccmd_param params;
+		params.parse_cmd(text);
+		
+		if (_execute_command(params.get_cmd(), params, session_ptr))
+		{
+			NORMAL_LOG("gm command: %s" , text);
+		}
+	}
+	void cgm_command::print_command(const std::string & cmd) const
+	{
+		for (NCOMMAND_MAP::const_iterator cit = m_gm_commands.begin(); cit != m_gm_commands.end(); ++cit)
+		{
+			if (cmd.empty() || cit->second.name.find(cmd) != std::string::npos)
+			{
+				NORMAL_LOG("%s, %s", cit->second.name.c_str() , cit->second.desc.c_str());
+			}
+		}
+	}
+	void cgm_command::_register_command(const std::string & cmd_name, gm_cmd_handler_type handler, const std::string & desc)
+	{
+		ncommand cmd(cmd_name, handler, desc);
+
+		if (false == m_gm_commands.insert(std::make_pair(cmd_name, cmd)).second)
+		{
+			ERROR_LOG("%s, insert command error");
+			return;
+		}
+	}
+	bool cgm_command::_execute_command(const std::string & cmd_name, const ccmd_param & param, csession * session_ptr)
+	{
+		//SYSTEM_LOG("find name = %s", cmd_name.c_str());
+		NCOMMAND_MAP::const_iterator ncmd = m_gm_commands.find(cmd_name);
+
+		if (ncmd == m_gm_commands.end())
+		{
+			ERROR_LOG(  "unknown command ");
+			return false;
+		}
+
+		if (!(ncmd->second.handler(param, session_ptr)))
+		{
+			ERROR_LOG( "execute failed " );
+			return false;
+		}
+
+		return true;
+	}
+}//namespace chen 
---
+++ cloud_platform/client_device/cplayer.h
@@ -0,0 +1,148 @@
+﻿/***********************************************************************************************
+				created: 		2019-05-12
+
+				author:			chensong
+
+				purpose:		palyer
+************************************************************************************************/
+
+
+#ifndef _C_PLAYER_H_
+#define _C_PLAYER_H_
+#include "cnoncopyable.h"
+#include "cnet_types.h"
+#include "ccmd_param.h"
+//#include "csession.h"
+//#include "cmgr_base.h"
+#include <google/protobuf/message_lite.h>
+//#include "cfriend_circle_table_mgr.h" //头文件在包含顺序上不要成闭合的环状，他们结构顺序最好应该是树。
+namespace chen {
+	class csession;   //告诉编译编译器 要先编译clan_session文件再编译cplayer
+	//class cmgr_base;
+	class cfriend_circle_table_mgr; // C++ 里面编译是比较奇葩的事情
+	class ctest_case_mgr;
+	class cmatch_game_mgr;
+	class cplayer : private cnoncopyable
+	{
+	public:
+		explicit cplayer();
+		~cplayer();
+	public:
+		bool init(csession *lan_session_ptr, uint32 index);
+		void destroy();
+		void update(uint32 msleep);
+	public:
+		void reset();
+
+		void on_logined(const std::string &user_name, int32 score, const std::string& token , int32 uid, uint32 login_session_id);
+	
+	public:
+		uint32 get_player_id() const { return m_web_user_id; }
+		std::string get_player_name() {
+			return m_user_name;
+		};
+        const std::string get_player_name() const {
+            return m_user_name;
+        };
+	public:
+		//得到玩家id
+		const uint32 get_session_id() const { return m_session_index; }
+		const csession * get_session() const {return m_session_ptr;}
+        csession * get_session()  {return m_session_ptr;}
+	public:
+		/*cfriend_circle_table_mgr* get_friend_circle_table_mgr() { return (cfriend_circle_table_mgr*)(m_mgrs[MGR_FIRENDCIRCLE]); }
+		cmatch_game_mgr* get_match_game_mgr() { return (cmatch_game_mgr*)(m_mgrs[MGR_MATCH_GAME]); }
+		ctest_case_mgr* get_test_case_mgr() { return (ctest_case_mgr*)(m_mgrs[MGR_TEST_CASE]); }*/
+		
+	public:
+		/*const cfriend_circle_table_mgr* get_friend_circle_table_mgr() const { return (const cfriend_circle_table_mgr*)(m_mgrs[MGR_FIRENDCIRCLE]); }
+		const cmatch_game_mgr* get_match_game_mgr()const { return (const cmatch_game_mgr*)(m_mgrs[MGR_MATCH_GAME]); }
+		const ctest_case_mgr* get_test_case_mgr() const  { return (const ctest_case_mgr*)(m_mgrs[MGR_TEST_CASE]); }*/
+	public:
+		bool send_msg(uint16_t msg_id, const ::google::protobuf::MessageLite& msg) const;
+		bool send_msg(uint16 msg_id, const ::google::protobuf::MessageLite& msg, int32 match_id);
+		bool send_gm(const char* buf) const;
+		void on_msg(uint16_t msg_id, int32 result);
+	public:
+		void 	handle_code(const void* packet, uint32 msg_size);
+		//void	handle_announce(const void* packet, uint32 msg_size);
+		//void	handle_breatheart(const void* packet, uint32 msg_size);
+		//void    handle_login(const void *packet, uint32 msg_size);
+		//void	handle_match_list(const void *packet, uint32 msg_size);
+		//void	handle_player_joinlist(const void *packet, uint32 msg_size);
+		//void	handle_match_run_list(const void *packet, uint32 msg_size);
+		//
+		//// friend circle
+		//void	handle_create_table(const void *packet, uint32 msg_size);
+		//void	handle_delete_table(const void *packet, uint32 msg_size);
+		//void    handle_modify_table(const void *packet, uint32 msg_size);
+
+
+		//// game
+		//void	handle_join_table(const void *packet, uint32 msg_size);
+		//void	handle_leave_table(const void *packet, uint32 msg_size);
+
+		//// game logic
+		//void	handle_game_info(const void *packet, uint32 msg_size);
+		//void	handle_game_timer_info(const void *packet, uint32 msg_size);
+		//void	handle_game_bonus(const void *packet, uint32 msg_size);
+		//void	handle_game_action(const void *packet, uint32 msg_size);
+		//void	handle_game_player_ranking(const void *packet, uint32 msg_size);
+		//void	handle_game_show_hand_card(const void *packet, uint32 msg_size);
+		//void	handle_match_add_chip(const void *packet, uint32 msg_size);
+		//void    handle_match_free_signup_num(const void *packet, uint32 msg_size);
+		//void	handle_game_lookon(const void *packet, uint32 msg_size);
+		//void	handle_game_player_back_chair(const void *packet, uint32 msg_size);
+		//void	handle_game_add_score(const void *packet, uint32 msg_size);
+		//void    handle_game_add_operationtimes(const void *packet, uint32 msg_size);
+  //      void    handle_game_lookon_players(const void *packet, uint32 msg_size);
+  //      void    handle_match_game_device(const void *packet, uint32 msg_size);
+		////        void    handle_game_player_operationtimes(const void *packet, uint32 msg_size);
+
+		//void	handle_match_info_update_notify(const void *packet, uint32 msg_size);
+		//void	handle_ante_blind_change_notify(const void *packet, uint32 msg_size);
+		//void	handle_match_start_notify(const void *packet, uint32 msg_size);
+		//void	handle_match_finish_notify(const void *packet, uint32 msg_size);
+		//void	handle_match_wait_notify(const void *packet, uint32 msg_size);
+		//void	handle_match_result_notify(const void *packet, uint32 msg_size);
+		//void	handle_match_cancle_notify(const void *packet, uint32 msg_size);
+		//void	handle_match_status_change_notify(const void *packet, uint32 msg_size);
+		//void	handle_match_add_chip_notify(const void * packet, uint32 msg_size);
+		//void	handle_game_ready_start_notify(const void *packet, uint32 msg_size);
+		//void	handle_game_start_notify(const void *packet, uint32 msg_size);
+		//void	handle_game_flop_cards_notify(const void *packet, uint32 msg_size);
+		//void	handle_game_turn_cards_notify(const void *packet, uint32 msg_size);
+		//void	handle_game_river_cards_notify(const void *packet, uint32 msg_size);
+		//void	handle_game_user_action_notify(const void *packet, uint32 msg_size);
+		//void	handle_game_user_handcard_notify(const void *packet, uint32 msg_size);
+		//void	handle_game_end_info_notify(const void *packet, uint32 msg_size);
+		//void	handle_game_show_hand_card_notify(const void *packet, uint32 msg_size);
+		//void	handle_game_user_sitedown_notify(const void *packet, uint32 msg_size);
+		//void	handle_game_user_leaveseat_notify(const void *packet, uint32 msg_size);
+		//void	handle_game_user_reservestandup_notify(const void *packet, uint32 msg_size);
+		//void	handle_match_player_cash_chage_notify(const void *packet, uint32 msg_size);
+		//void    handle_game_user_add_operationtimes_notify(const void * packet, uint32 msg_size);
+		//void    handle_match_operationtimes_status_notify(const void * packet, uint32 msg_size);
+		//void    handle_match_multianguage_notify(const void * packet, uint32 msg_size);
+  //      void    handle_game_lookon_players_num_notify(const void * packet, uint32 msg_size);
+  //      void    handle_match_table_count_notify(const void * packet, uint32 msg_size);
+  //      void    handle_game_device_notify(const void * packet, uint32 msg_size);
+
+  //      void    handle_game_end_add_chip_notice(const void *packet, uint32 msg_size);
+
+
+
+	private:
+		csession *				m_session_ptr;
+		//cmgr_base*				m_mgrs[MGR_MAX];
+		uint32					m_session_index;
+		std::string				m_user_name;
+		int32					m_score;
+		std::string				m_token;
+		int32					m_web_user_id;
+		uint32					m_login_session_id; //http 登录时回话id
+	};
+
+}// namespace chen
+
+#endif // !#define _C_PLAYER_H_
